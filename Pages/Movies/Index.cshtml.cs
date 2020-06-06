359bf2648195d12e5a495c21a650d3bd3c180cd9
AmirHossein Parsapour <ah.parsapour@gmail.com>
2020-06-06 19:47:04 +0430

    first commit

---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get;set; }
+        
+        public SelectList Generes { get;set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
